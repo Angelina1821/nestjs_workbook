4f92ceb724c1d66efb07f3c5884ee0d535f29b82
Ангелина Цой <82329206+Angelina1821@users.noreply.github.com>
2026-09-15 13:52:31 +0300

    refactor: remove unused workshop repository module dependency

--- src/bookings/bookings.module.ts
+++ src/bookings/bookings.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Workshop } from '../workshops/entities/workshop.entity';
 import { Booking } from './entities/booking.entity';
 import { BookingsController } from './bookings.controller';
 import { BookingsService } from './bookings.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Booking, Workshop])],
+  imports: [TypeOrmModule.forFeature([Booking])],
   controllers: [BookingsController],
   providers: [BookingsService],
 })
