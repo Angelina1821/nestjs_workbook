741660ff6179a95764203eba90c439c8fdfa5ac1
Ангелина Цой <82329206+Angelina1821@users.noreply.github.com>
2026-09-15 13:00:15 +0300

    feat: add registration validation DTO

---
+++ src/users/dto/register.dto.ts
@@ -0,0 +1,12 @@
+import { IsString, Length, Matches } from 'class-validator';
+
+export class RegisterDto {
+  @IsString()
+  @Length(3, 150)
+  username!: string;
+
+  @IsString()
+  @Length(8, 128)
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d).+$/, { message: 'Пароль должен содержать буквы и цифры.' })
+  password!: string;
+}
